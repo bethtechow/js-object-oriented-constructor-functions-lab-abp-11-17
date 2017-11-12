b40bebf3fb0d97d0318dfc718db62d37526b93ca
bethtechow <bethtechow@yahoo.com>
2017-11-12 19:59:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-function objects {
-  constructor(Scooter: year, color, model){
-    
-  }
+function Scooter (year, color, model) {
+  this.year=year;
+    this.color=color;
+    this.model=model;
 
 }
+
+var Samson = new Scooter (1998, "red", "vespa")
